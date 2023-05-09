ea7059b43acb8a02dd91f5a74adfa711f78b750c
PRAMOD R <85751742+pramodr18@users.noreply.github.com>
2023-05-09 22:36:44 +0530

    Add files via upload

---
+++ joke.js
@@ -0,0 +1,39 @@
+function genarate() {
+  let jock = [
+    "What does Jeff Bezos do before he goes to sleep? He puts his PJ-Amazon!",
+
+    "What happened when the world's tongue-twister champion got arrested? They gave him a tough sentence!",
+
+    "What did the mama cow say to the calf? It’s pasture bedtime!",
+
+    "How does a vampire start a letter? Tomb it may concern!",
+
+    "What do you call an illegally parked frog? Toad!",
+
+    "What did one plate say to the other? Dinner is on me!",
+
+    "Why do hummingbirds hum? Because they don’t know the words!",
+
+    "What do sprinters eat before a race? Nothing. They fast!",
+
+    "Two muffins are baking in an oven. One of them looks to the other and says, Phew, it's getting hot in here! The other looks back and says, Ack! A talking muffin!",
+    "What do cats eat for breakfast? Mice Crispies!",
+
+    "What do you call an elephant that doesn’t matter? An irrelephant!",
+
+    "How do you keep a bull from charging? Take away its credit card!",
+
+    "why can’t a leopard hide? because he’s always spotted!",
+    "ಹುಡುಗ ಹುಡುಗಿಯನ್ನು ಚುಡಾಯಿಸುತ್ತಾ.... ಹುಡುಗ: ನೀನು 94% ಸೈಲೆಂಟ್, 95% ಚೆಲುವೆ, 96% ಆಕರ್ಷಕವಾಗಿದ್ದೀಯಾ.. ಹುಡುಗಿ: ಆ ಮೇಲೆ.. ಹುಡುಗ: 97% ಕ್ಯೂಟ್ , 98% ಸ್ವೀಟ್, 99% ಸ್ಮಾರ್ಟ್.. ಆ ಮೇಲೆ: ನಾನು ಮೇಲೆ ಹೇಳಿದ್ದು 100% ಜೋಕ್",
+    "ಪತ್ನಿ : (ಕನ್ನಡಿಯಲ್ಲಿ ತನ್ನ ಮುಖವನ್ನು ನೋಡುತ್ತಾ) ಛೇ.., ನೋಡೋಕೆ ನಾನು ಎಷ್ಟು ಗಲೀಜಾಗಿದ್ದೀನಲ್ಲಾ? ರೀ ಏನಾದ್ರೂ ನನ್ನ ಬಗ್ಗೆ ಒಳ್ಳೆ ಮಾತಾಡ್ರೀ... ಪತಿ : ನಿನ್ನ ಐ ಸೈಟ್ ಕರೆಕ್ಟಾಗಿದೆ",
+    "* ಆಧಾರ್ ಲಿಂಕ್ ಅಕೌಂಟ್‌ಗೆ ಹೋಗಿದೆ ಅಪ್ಪ; ರಿಸಲ್ಟ್ ಏನೋ ಇದು ಕೇವಲ 40 ಪರ್ಸೆಂಟ್ ಬಂದಿದೆ. ಮಗ; ಇಲ್ಲಪ್ಪ ಶೇ 80ರಷ್ಟು ಬಂದಿದೆ 40 ಮಾತ್ರ ಕಾಣಿಸ್ತಾ ಇದೆ ಅಪ್ಪ; ಉಳಿದ 40 ಪರ್ಸೆಂಟ್ ಎಲ್ಲಿ ಹೋಯಿತು ಮಗ; ಆಧಾರ್ ಕಾರ್ಡ್‌ ಲಿಂಕ್ ಮಾಡಿದ ಬ್ಯಾಂಕ್ ಖಾತೆಗೆ ಜಮಾ ಮಾಡ್ತಿನಿ ಅಂತ ಹೇಳಿದಾರೆ.",
+    "ಕೊನೆ ಆಸೆ ಕೇಳಿ ಜೈಲರ್ ಸುಸ್ತು ಜೈಲರ್; ನಾಳೆ ನಿನ್ನನ್ನು ಗಲ್ಲಿಗೇರಿಸುತ್ತೇವೆ, ಕೊನೆ ಆಸೆ ಏನು ಹೇಳು? ಕೈದಿ; ತಲೆ ಕೆಳಗೆ ಮಾಡಿ, ಕಾಲು ಮೇಲೆ ಮಾಡಿ ನೇಣು ಹಾಕಿ",
+    "* ನಾನು ಕೆಲಸ ಬಿಟ್ಟು ನೊಣಕ್ಕೆ ಈಜು ಕಲಿಸಲ ಗ್ರಾಹಕ; ಯಾರು ಈ ಹೋಟೆಲ್‌ ಮ್ಯಾನೇಜರ್, ಕಾಫಿಯಲ್ಲಿ ನೊಣ ಬಿದ್ದು ಸತ್ತಿದೆ? ಮ್ಯಾನೇಜರ್; ಸರ್ ನಾನು ನನ್ನ ಕೆಲಸ ಬಿಟ್ಟು ನೊಣಕ್ಕೆ ಈಜುವುದು ಕಲಿಸಲಾ?",
+    "* ಶಿಕ್ಷಕರು ಎಂದರೆ GST ಅಪ್ಪ; ನೀವು ಶಿಕ್ಷಕರನ್ನು ಏನಂತ ಕರೀತಿರೋ? ಮಗ; GST ಅಪ್ಪ; ಏ ಸರಿಯಾಗಿ ಹೇಳು ಮಗ; ಹೌದಪ್ಪ Government School Teacher",
+    "* ಜಾಹೀರಾತು ಕೊಡ್ತಿನಿ ಗಂಡ; ನಾನು ತಪ್ಪಿಸಿಕೊಂಡರೆ ನೀನು ಏನು ಮಾಡ್ತಿಯಾ? ಹೆಂಡತಿ; ಜಾಹೀರಾತು ಕೊಡ್ತಿನಿ ಗಂಡ; ಏನಂತ ಜಾಹೀರಾತು? ಗಂಡ; ಗಂಡ ಬೇಕಾಗಿದ್ದಾನೆ ಎಂದು.",
+    "* ಬಾಟಲ್ ಕೊಟ್ಟು ಬಂದೆ ಗುಂಡ; ಎಣ್ಣೆ ಬಾಟಲ್ ತರುವಾಗ ಅಪ್ಪ ಎದುರಿಗೆ ಬಂದ್ರು ಸ್ನೇಹಿತ; ಸಿಕ್ಕಿಬಿದ್ಯಾ, ಏನ್‌ ಮಾಡಿದೆ? ಗುಂಡ ; ಬಾಟಲ್ ಕೈಗೆ ಕೊಟ್ಟು ಹ್ಯಾಪಿ ಬರ್ತಡೇ ಡ್ಯಾಡಿ ಅಂತ ಹೇಳಿ ಬಂದೆ.",
+  ];
+  let n = jock.length;
+  let rand = Math.floor(Math.random() * n - 1);
+  document.getElementById("content").innerHTML = jock[rand];
+}
